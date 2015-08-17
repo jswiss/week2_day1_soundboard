221cb4b41f6cca35bcb4b2c58c89deb07f7a9e97
Joshua Swiss <swissjoshua@gmail.com>
2015-08-17 22:19:25 +0100

    final design additions

--- js/js.js
+++ js/js.js
@@ -1,26 +1,22 @@
-console.log('howdy friend');
-
+//holds click action until window loads
 window.onload = function (){
   var clickPlay = document.getElementById('soundbox');
   clickPlay.addEventListener('click', playSound);
-
 }
 
+//array of mp3s
 var mp3s = ['sounds/one.mp3','sounds/two.mp3', 'sounds/three.mp3', 'sounds/four.mp3', 'sounds/five.mp3', 'sounds/six.mp3', 'sounds/seven.mp3', 'sounds/eight.mp3'];
 
-var randMp3 = mp3s[Math.floor(Math.random()*mp3s.length)];
-// randmp3 = a song
+// //random generation of 
+// var randMp3 = mp3s[Math.floor(Math.random()*mp3s.length)];
 
 soundManager.setup({
-
   url: '/swf/',
-
-  // good to go: the onready() callback
-
   onready: function() {
   }
 });
 
+//plays sound, randomly generating new clip with each click
 function playSound(){
   var randMp3 = mp3s[Math.floor(Math.random() * mp3s.length)];  
   var mySound = soundManager.createSound({
